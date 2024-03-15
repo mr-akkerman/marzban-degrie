49fa3099fab21caf2f1716ce6a53c7affef3bd98
ivan.akkerman <akkerman.live@gmail.com>
2024-03-15 22:48:45 +0700

    Fixed path

--- install.sh
+++ install.sh
@@ -128,6 +128,7 @@ install_marzban_script() {
 install_marzban() {
     # Fetch releases
     FILES_URL_PREFIX="https://github.com/mr-akkerman/marzban-degrie.git/dev"
+    FILES_URL_PREFIX="https://raw.githubusercontent.com/mr-akkerman/marzban-degrie.git/dev"
 
     mkdir -p "$DATA_DIR"
     mkdir -p "$APP_DIR"
